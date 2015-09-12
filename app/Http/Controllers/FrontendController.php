3c10684e17ab802460a2d9543dcc6650684fe798
nhunght <nhunghoang1609@gmail.com>
2015-09-12 18:24:30 +0700

    first commit

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class FrontendController extends Controller
+{
+
+   public function index(){
+   	return view('frontend.index');
+   }
+}
